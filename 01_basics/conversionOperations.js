b8f8cb3508a69e0f94e05aff5a5ff600c927462b
Harsh Sharma <103068420+ImHarshSharma@users.noreply.github.com>
2024-01-21 10:05:48 +0000

    operations and conversions

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,80 @@
+let score = 33;
+// console.log(typeof score)
+
+let stringScore = String(score)
+// console.log( typeof stringScore)
+// console.log(stringScore)
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "harsh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " harsh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+// <<<<<<<<<<--------------Increment and Decrement------------------------>>>>>>>>>>>>>>>>>>
+
+
+// for increment : ->
+// at postfix
+
+let a = 3;
+let b = a++;
+
+console.log(`a: ${a} , b: ${b}`);
+
+// at prefix 
+
+let x = 5;
+let y = ++x;
+
+console.log(`x : ${x} , y : ${y}`)
