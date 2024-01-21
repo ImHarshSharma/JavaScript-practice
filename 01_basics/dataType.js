ded40ce60359a9508cd4d98fa48412922653868a
Harsh Sharma <103068420+ImHarshSharma@users.noreply.github.com>
2024-01-21 09:04:47 +0000

    DataTypes&Variable

---
+++ 01_basics/dataType.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let StudentName="harsh sharma"  // this is the string type
+let rollNo = 123    // number type
+let present = true            // it can be true/false
+
+console.log(typeof StudentName)
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const EmpId = 1243
+
+let EmpName = "Harsh Sharma"
+let EmpPassword = "452624"
+let EmpCity = "Bhiwani"
+
+
+console.table([EmpId,EmpName,EmpPassword,EmpCity])
