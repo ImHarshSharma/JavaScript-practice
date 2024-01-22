14f9149c5381af9dc0c765e7046a466fbe32d420
Harsh Sharma <103068420+ImHarshSharma@users.noreply.github.com>
2024-01-22 06:12:12 +0000

    DataTypes2

---
+++ 01_basics/dataTypes2.js
@@ -0,0 +1,40 @@
+const a = 123;
+const b = 214.43;
+const isReady = true;
+const temp = null;
+let age;
+
+// console.log(age)
+
+
+const id = Symbol("12");
+const anotherId = Symbol("12");
+
+
+// console.log(id == anotherId);
+
+
+const bigNumber = 647553653637n;
+
+// console.log(typeof bigNumber)
+
+const stName = ["harsh", "aditya", "abhishek"];
+let myObj = {
+    name: "harsh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof isReady);
+console.log(typeof temp);
+console.log(typeof age);
+console.log(typeof bigNumber);
+console.log(typeof stName);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+
